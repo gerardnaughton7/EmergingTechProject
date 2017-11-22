893e5ce38d8c922bd6d0d3074c59eef0ef5d3d3f
gerard naughton <gerard naughton>
2017-11-22 17:56:21 +0000

    Created a shell with flask, css, javascript and html to draw a number on page

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
-from flask import Flask, render_template, request
-from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
+import os
+from flask import Flask, request, redirect, url_for, flash, render_template
 
 app = Flask(__name__)
 
-photos = UploadSet('photos', IMAGES)
-
-app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
-configure_uploads(app, photos)
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST' and 'photo' in request.files:
-        filename = photos.save(request.files['photo'])
-        return filename
-    return render_template('upload.html')
-
-
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    return render_template("index.html")
+ 
 if __name__ == '__main__':
-    app.run(debug=True)
+        app.run()
